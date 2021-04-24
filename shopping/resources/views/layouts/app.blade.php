934455ccc42cd84107b80ada30a8048805e8a9d5
maie421 <maie421@naver.com>
2021-04-24 14:43:20 +0900

    기본 화면

--- shopping/resources/views/layouts/app.blade.php
+++ shopping/resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
--- shopping/tailwind.config.js
+++ shopping/tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.js',
     ],
-
+    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             fontFamily: {
